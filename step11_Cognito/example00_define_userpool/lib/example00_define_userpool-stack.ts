0c845a55f6831de91a9e27257f12c53d70b03a43
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-11-11 18:04:08 +0500

    Update example00_define_userpool-stack.ts

--- step11_Cognito/example00_define_userpool/lib/example00_define_userpool-stack.ts
+++ step11_Cognito/example00_define_userpool/lib/example00_define_userpool-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from "@aws-cdk/core";
+import * as cognito from "@aws-cdk/aws-cognito";
 
 export class Example00DefineUserpoolStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
