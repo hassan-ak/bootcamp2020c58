406adc33904c39dc8d68a29a4355c84098b4624d
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-11-11 18:26:17 +0500

    Create new user pool

--- step11_Cognito/example00_define_userpool/lib/example00_define_userpool-stack.ts
+++ step11_Cognito/example00_define_userpool/lib/example00_define_userpool-stack.ts
@@ -6,5 +6,40 @@ export class Example00DefineUserpoolStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    const userPool = new cognito.UserPool(this, "TestUserPool", {
+      userPoolName: "Test User Pool",
+      selfSignUpEnabled: true, // Allow users to sign up
+      autoVerify: { email: true }, // Verify email addresses by sending a verification code
+      signInAliases: { email: true }, // Set email as an alias
+      userVerification: {
+        emailSubject: "Verify your email for our awesome app!",
+        emailBody:
+          "Hello {username}, Thanks for signing up to our awesome app! Your verification code is {####}",
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+      }, ///customize email and sms
+      standardAttributes: {
+        fullname: {
+          required: true,
+          mutable: false,
+        },
+      }, ////Attributes already define by cognito
+      customAttributes: {
+        myappid: new cognito.StringAttribute({
+          minLen: 5,
+          maxLen: 15,
+          mutable: false,
+        }),
+      }, ////Custom Attributes defined according to the application needs
+      passwordPolicy: {
+        minLength: 12,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireDigits: true,
+        requireSymbols: true,
+        tempPasswordValidity: cdk.Duration.days(3),
+      }, ///Password Policy
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY, ///Account Recovery email
+    });
   }
 }
