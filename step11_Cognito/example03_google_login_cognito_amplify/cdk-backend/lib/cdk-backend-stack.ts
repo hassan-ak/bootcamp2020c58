588c7742e3d7518b65c98f454d10da37b1c83e12
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-11-23 01:46:03 +0500

    create userpool

--- step11_Cognito/example03_google_login_cognito_amplify/cdk-backend/lib/cdk-backend-stack.ts
+++ step11_Cognito/example03_google_login_cognito_amplify/cdk-backend/lib/cdk-backend-stack.ts
@@ -1,15 +1,27 @@
-import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import * as cognito from "@aws-cdk/aws-cognito";
 
 export class CdkBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // User Pool
+    const userPool = new cognito.UserPool(this, "step03UserPool", {
+      userPoolName: "Step03 Google Login User Pool",
+      selfSignUpEnabled: true,
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      userVerification: {
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+      },
+      autoVerify: {
+        email: true,
+      },
+      standardAttributes: {
+        email: {
+          required: true,
+          mutable: true,
+        },
+      },
+    });
   }
 }
