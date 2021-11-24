521aa0d62d4a298193c2f281d70bdabacaa23851
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-11-24 15:53:22 +0500

    create amplify hub

--- step11_Cognito/example03_google_login_cognito_amplify/frontend/Readme.md
+++ step11_Cognito/example03_google_login_cognito_amplify/frontend/Readme.md
@@ -104,4 +104,32 @@
     }
     ```
 
-18.
+18. Create a aws Hub to listen to data
+
+    ```javascript
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+      Hub.listen("auth", ({ payload: { event, data } }) => {
+        switch (event) {
+          case "signIn":
+          case "cognitoHostedUI":
+            getUser().then((userData) => setUser(userData));
+            break;
+          case "signOut":
+            setUser(null);
+            break;
+          case "signIn_failure":
+          case "cognitoHostedUI_failure":
+            console.log("Sign in failure", data);
+            break;
+        }
+      });
+
+      getUser().then((userData) => {
+        setUser(userData);
+        console.log("Signed In:", userData);
+      });
+    }, []);
+    ```
+
+19.
--- step11_Cognito/example03_google_login_cognito_amplify/frontend/src/pages/index.tsx
+++ step11_Cognito/example03_google_login_cognito_amplify/frontend/src/pages/index.tsx
@@ -1,10 +1,34 @@
-import React from "react";
-import Amplify, { Auth } from "aws-amplify";
+import React, { useEffect, useState } from "react";
+import Amplify, { Auth, Hub } from "aws-amplify";
 import awsmobile from "../aws-exports";
 
 Amplify.configure(awsmobile);
 
 export default function Home() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    Hub.listen("auth", ({ payload: { event, data } }) => {
+      switch (event) {
+        case "signIn":
+        case "cognitoHostedUI":
+          getUser().then((userData) => setUser(userData));
+          break;
+        case "signOut":
+          setUser(null);
+          break;
+        case "signIn_failure":
+        case "cognitoHostedUI_failure":
+          console.log("Sign in failure", data);
+          break;
+      }
+    });
+
+    getUser().then((userData) => {
+      setUser(userData);
+      console.log("Signed In:", userData);
+    });
+  }, []);
+
   function getUser() {
     return Auth.currentAuthenticatedUser()
       .then((userData) => userData)
